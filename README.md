2b6aaf5f5845ea2b4d8e811c56a44b807aaf7edc
unknown <amalnnath20@yahoo.ca>
2021-11-14 17:00:38 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# PyPainter
+
+## Execution
+Make sure you have Python3 installed on your machine and a Windows PC with paint. This can work with other drawing apps but will probably need a bit more modification.
+
+This script should work out of the box for a machine with a resolution of 1920x1080 given that paint is fullscreen. Otherwise, there are certain coordinates marked in the script that will need to be changed. Commenting out the while loop in the __main__ function should help you determine the coordinates. Make sure the pen tool is already selected in paint prior to execution.
+
+To execute the project, run:
+```
+pip install -r requirements.txt
+python script.py
+```
---
+++ script.py
@@ -0,0 +1,89 @@
+import cv2
+import pyautogui
+
+def load_bw_img(filename):
+    original = cv2.imread(filename)
+    grayImage = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
+    _ ,blackAndWhiteImage = cv2.threshold(grayImage, 127, 255, cv2.THRESH_BINARY)
+    return blackAndWhiteImage
+
+# Pixel by Pixel B/W
+def draw_slow(bw, x_coord, y_coord):
+    pyautogui.moveTo(x_coord, y_coord)
+    y = y_coord
+    for col in bw:
+        x = x_coord  
+        y += 1
+        for i in col:
+            x += 1
+            if i != 255:
+                pyautogui.moveTo(x, y)
+                pyautogui.click()
+
+# Pixel by pixel in color
+def draw_color(img, x_coord, y_coord):
+    last_color = None
+    pyautogui.moveTo(x_coord, y_coord)
+    y = y_coord
+    for col in img:
+        x = x_coord  
+        y += 1
+        for i in col:
+            b, g, r = (i)
+            x += 1
+            if last_color != [r, g, b] or last_color == None:
+                last_color = [r, g, b]
+                change_color(r, g, b)
+            if [r, g, b] != [255, 255, 255]:
+                pyautogui.moveTo(x, y)
+                pyautogui.click()
+
+# Draws as lines in black and white
+def draw_faster(bw, x_coord, y_coord):
+    pyautogui.moveTo(x_coord, y_coord)
+    y = y_coord
+    for col in bw:
+        x = x_coord  
+        y += 1
+        is_black = False
+        f_black = None
+        for i in col:
+            if i == 255:
+                if is_black:
+                    pyautogui.moveTo(f_black)
+                    pyautogui.mouseDown(button='left')
+                    pyautogui.moveTo(x, y)
+                    pyautogui.mouseUp(button='left')
+                    is_black = False
+                    f_black = None
+            else:
+                if not is_black:
+                    is_black = True
+                    f_black = (x, y)
+            x += 1
+
+# Function to change color
+def change_color(r, g, b):
+    edit_color = (1000, 75) # Revise with coordinates for edit_colors button for your machine
+    r_value = (1158, 595) # Revise with coordinates for R field for your machine
+    pyautogui.click(edit_color)
+    pyautogui.doubleClick(r_value)
+    pyautogui.write(str(r))
+    pyautogui.press('tab')
+    pyautogui.write(str(g))
+    pyautogui.press('tab')
+    pyautogui.write(str(b))
+    pyautogui.press('enter')
+
+
+if __name__ == "__main__":
+    x = 30 # Change depending on X/Y coordinates of your machine for where to start drawing
+    y = 170
+    bw_img = load_bw_img("ksi.jpg")
+    # img = cv2.imread("ksi2.jpg", cv2.IMREAD_UNCHANGED)
+    while True:
+        print(pyautogui.position()) # Uncomment this to find coordinates
+
+    #draw_slow(bw_img, x, y)
+    draw_faster(bw_img, x, y)
+    # draw_color(img, x, y)
